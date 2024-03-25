a564569ec2fba278db36cc8439ad001db9e8bb27
Kim Hyun Deok <khd1323@naver.com>
2024-03-25 22:30:48 +0900

    Game 프로젝트 Engine 라이브러리 의존성 추가

--- Game/MyGame.cpp
+++ Game/MyGame.cpp
@@ -1,16 +1,16 @@
 #include "Game.h"
-
+#pragma comment(lib, "Engine.lib")
 class MyGame : public Engine::Game
 {
 public:
-	MyGame() {}
-
+    MyGame() {}
+    ~MyGame() {}
 };
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
     PSTR lpCmdLine, int nCmdShow)
 {
     MyGame myGame;
-
+    myGame.Run(hInstance, hPrevInstance, lpCmdLine, nCmdShow);
     return 0;
 }
