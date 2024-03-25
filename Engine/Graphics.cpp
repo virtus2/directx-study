eb44a0d594ab8edd96198ad66628b14f0a4b6002
Kim Hyun Deok <khd1323@naver.com>
2024-03-25 23:44:36 +0900

    Engine 그래픽 클래스 추가

--- Engine/Graphics.cpp
+++ Engine/Graphics.cpp
@@ -1,2 +1,15 @@
 #include "pch.h"
 #include "Graphics.h"
+
+Graphics::Graphics()
+{
+}
+
+Graphics::~Graphics()
+{
+}
+
+int Graphics::Initialize(HWND hWnd, int width, int height)
+{
+	return 0;
+}
--- Engine/Graphics.h
+++ Engine/Graphics.h
@@ -1,5 +1,17 @@
-#pragma once
+﻿#pragma once
+#include "pch.h"
+
 class Graphics
 {
+public:
+	Graphics();
+	~Graphics();
+
+	int Initialize(HWND hWnd, int width, int height);
+	void BeginDraw();
+	void EndDraw();
+	void Clear();
+	void Draw();
+	void Present();
 };
 
