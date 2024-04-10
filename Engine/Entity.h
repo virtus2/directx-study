a3e646049dd8eae8eeced27c141145aa9f155a24
Kim Hyun Deok <khd1323@naver.com>
2024-04-10 22:24:47 +0900

    Engine 엔티티 transform 관련 필드, 함수 추가

--- Engine/Entity.h
+++ Engine/Entity.h
@@ -1,5 +1,5 @@
 ﻿#pragma once
-
+#include "Matrix.h"
 class Model;
 
 class Entity
@@ -17,7 +17,19 @@ public:
 	void SetModel(std::shared_ptr<Model> model) { this->model = model; }
 	std::shared_ptr<Model> GetModel() { return model; }
 
+	void SetPosition(Math::Vector3 position) { this->position = position; }
+	void SetRotation(Math::Vector3 rotation) { this->rotation = rotation; }
+	void SetScale(Math::Vector3 scale) { this->scale = scale; }
+
+	Math::Vector3 GetPosition() { return position; }
+	Math::Vector3 GetRotation() { return rotation; }
+	Math::Vector3 GetScale() { return scale; }
+
 protected:
+	Math::Vector3 position = Math::Vector3::Zero();
+	Math::Vector3 rotation = Math::Vector3::Zero();
+	Math::Vector3 scale = Math::Vector3::One();
+
 	std::shared_ptr<Model> model;
 
 };
