64ce29eb32f1816b6e224637bdb141c6ba7f18ed
Kim Hyun Deok <khd1323@naver.com>
2024-04-11 22:59:01 +0900

    Engine 텍스처 클래스 생성자 및 소멸자 추가

--- Engine/Texture.cpp
+++ Engine/Texture.cpp
@@ -3,6 +3,16 @@
 #include "Texture.h"
 #include "Graphics.h"
 
+Texture::Texture()
+{
+
+}
+
+Texture::~Texture()
+{
+
+}
+
 void Texture::CreateTexture(Graphics* graphics, const std::wstring& filePath)
 {
 	graphics->CreateTexture(filePath, texture.GetAddressOf(), shaderResourceView.GetAddressOf(), samplerState.GetAddressOf());
